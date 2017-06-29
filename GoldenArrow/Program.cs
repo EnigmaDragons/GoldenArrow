d8d1d052b532d4d3b255964f840ecececd0af5c7
Silas Reinagel <silas.reinagel@gmail.com>
2017-06-28 22:25:36 -0700

    Created project/solution. Closes #1

---
+++ GoldenArrow/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Input;
+using MonoDragons.Core.EngimaDragons;
+using MonoDragons.Core.Engine;
+using MonoDragons.Core.Inputs;
+using MonoDragons.Core.Render;
+using MonoDragons.Core.Scenes;
+using MonoGame.Cards.Scenes;
+
+namespace GoldenArrow
+{
+#if WINDOWS || LINUX
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new NeedlesslyComplexMainGame("Golden Arrow", "Table", new Display(1600, 900, false, 1), CreateSceneFactory(), CreateController()))
+                game.Run();
+        }
+
+        private static IController CreateController()
+        {
+            return new KeyboardController(new Map<Keys, Control>
+            {
+                { Keys.Z, Control.A },
+            });
+        }
+
+        private static SceneFactory CreateSceneFactory()
+        {
+            return new SceneFactory(
+                new Dictionary<string, Func<IScene>>
+                {
+                    { "Logo", () => new LogoScene() },
+                    { "Table", () => new Table() },
+                });
+        }
+    }
+#endif
+}
---
+++ MonoDragons.Core/Render/SpriteRenderer.cs
@@ -0,0 +1,15 @@
+﻿using MonoDragons.Core.Engine;
+using MonoDragons.Core.Entities;
+using MonoDragons.Core.PhysicsEngine;
+
+namespace MonoDragons.Core.Render
+{
+    public sealed class SpriteRenderer : IRenderer
+    {
+        public void Draw(IEntities entities)
+        {
+            entities.ForEach(e => e.With<Sprite>(
+                sprite => World.Draw(sprite.Name, e.Transform)));
+        }
+    }
+}
