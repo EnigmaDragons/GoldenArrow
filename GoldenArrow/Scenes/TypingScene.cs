ac807643e3b1d20bd5a2af2ea4ce399e305ade72
Silas Reinagel <silas.reinagel@gmail.com>
2017-06-29 23:14:33 -0700

    Created TypingInput and KeyboardInput system. Closes #34

---
+++ GoldenArrow/Scenes/TypingScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using MonoDragons.Core.Entities;
+using MonoDragons.Core.KeyboardControls;
+using MonoDragons.Core.PhysicsEngine;
+using MonoDragons.Core.Render;
+using MonoDragons.Core.Scenes;
+
+namespace GoldenArrow.Scenes
+{
+    public class TypingScene : EcsScene
+    {
+        protected override IEnumerable<GameObject> CreateObjs()
+        {
+            yield return Entity
+                .Create(new Transform2(new Size2(200, 100)))
+                .Add(new TypingInput { IsActive = true })
+                .Add(x => new TextDisplay {Text = () => x.Get<TypingInput>().Value});
+            yield return Entity
+                .Create(new Transform2(new Vector2(300, 0), new Size2(200, 100)))
+                .Add(new TypingInput())
+                .Add(x => new TextDisplay { Text = () => x.Get<TypingInput>().Value });
+        }
+    }
+}
