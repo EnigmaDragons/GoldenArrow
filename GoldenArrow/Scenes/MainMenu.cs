56cf378de184f0cd6a5c502675ffe32ee53b7d58
Silas Reinagel <silas.reinagel@gmail.com>
2017-06-29 19:05:15 -0700

    Created EcsScene. Closes #28

--- GoldenArrow/Scenes/MainMenu.cs
+++ GoldenArrow/Scenes/MainMenu.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Microsoft.Xna.Framework;
-using MonoDragons.Core.Engine;
 using MonoDragons.Core.Entities;
 using MonoDragons.Core.MouseControls;
 using MonoDragons.Core.PhysicsEngine;
@@ -9,42 +7,20 @@ using MonoDragons.Core.Scenes;
 
 namespace GoldenArrow.Scenes
 {
-    public class MainMenu : IScene
+    public class MainMenu : EcsScene
     {
-        private readonly List<GameObject> _objs = new List<GameObject>();
-
-        public void Create()
+        protected override IEnumerable<GameObject> CreateObjs()
         {
-            _objs.Add(Entity
-                .Create(new Transform2(new Size2(1600, 900)))
-                .Add(new Sprite("Images/Menu/mainmenu")));
-            _objs.Add(Entity
+            yield return Entity
+               .Create(new Transform2(new Size2(1600, 900)))
+               .Add(new Sprite("Images/Menu/mainmenu"));
+            yield return Entity
                 .Create(new Transform2(new Vector2(300, 90), new Size2(1000, 171)))
-                .Add(new Sprite("Images/Menu/maintitle")));
-            _objs.Add(Entity
+                .Add(new Sprite("Images/Menu/maintitle"));
+            yield return Entity
                 .Create(new Transform2(new Vector2(700, 650), new Size2(200, 90)))
                 .Add(new Sprite("Images/Menu/startgame"))
-                .Add(new ClickAction(StartGame)));
-        }
-
-        private void StartGame()
-        {
-            Entity.Destroy(_objs);
-            World.NavigateToScene("Table");
-        }
-
-
-        public void Init()
-        {
-            Create();
-        }
-
-        public void Update(TimeSpan delta)
-        {
-        }
-
-        public void Draw()
-        {
+                .Add(new ClickAction(() => NavigateToScene("Table")));
         }
     }
 }
